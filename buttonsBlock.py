66d67d15787894b78b14ab84d18e2a2161866180
benevolentimp <jaakkola.jooa@gmail.com>
2022-03-21 20:55:46 +0200

    Until < Lab_5

--- buttonsBlock.py
+++ buttonsBlock.py
@@ -3,6 +3,7 @@ Title:          [file.py]
 Author:         Jooa Jaakkola (@benevolentimp)
 Description:    ["Free-form simplification"]
 """
+import tkinter as tk
 from tkinter import ttk
 import math
 
@@ -17,26 +18,39 @@ class ButtonsBlock(Block):
         super().__init__(parent)  # Pass information to Block.
         self.parent = parent
         self.buttons = []  # To collect the buttons from Frame.
+        self.tvar = [''] * len(parent.game.board)
         self.__createLayout()
 
     def __createLayout(self):
         """
         No need for outsiders to make stuff, so hidden.
         """
-        x, y, j = 0, 0, 0
-        for item in self.parent.game.board:
-            button = ttk.Button(self, text=item, command=lambda j=j: self.parent.move(j))
-            button.grid(row=x, column=y)
-            y += 1
-            j += 1
-            # If n.of columns == sqrt(len(game.board)) => column iteration "to next line"
-            if y == int(math.sqrt(len(self.parent.game.board))):
-                y = 0
-                x += 1
+
+        rows = int(math.sqrt(len(self.parent.game.board)))
+        self.buttons = []  # remove previous
+        for i in range(0, len(self.parent.game.board)):
+            t = tk.StringVar()
+            self.tvar[i] = t
+            self.tvar[i].set(self.parent.game.board[i])
+            button = ttk.Button(self, textvariable=self.tvar[i], \
+                                command=lambda i=i: self.parent.move(i))
+            button.grid(row=i // rows, column=i % rows, sticky=tk.NSEW)
+            self.rowconfigure(i // rows, weight=1)
+            self.columnconfigure(i % rows, weight=1)
+            self.buttons.append(button)
+            # x, y, j = 0, 0, 0
+        # for item in self.parent.game.board:
+        #     button = ttk.Button(self, text=ttk.Label(), command=lambda j=j: self.parent.move(j))
+        #     button.grid(row=x, column=y)
+        #     y += 1
+        #     j += 1
+        #     # If n.of columns == sqrt(len(game.board)) => column iteration "to next line"
+        #     if y == int(math.sqrt(len(self.parent.game.board))):
+        #         y = 0
+        #         x += 1
 
     def disable(self, nro):  # Overwrites abstract-method as it should.
-        pass
-
+        self.buttons[nro].config(state="disabled")
 
 # testing
 # btn = ButtonsBlock()
--- minigamesGUI_ver1.py
+++ minigamesGUI_ver1.py
@@ -8,57 +8,102 @@ Description:    Graphical user-interface.
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
+import math
 
 # From PATH:
 from minigames.memorygame import Memorygame
 from minigames.fiverow import FiveRow
+from minigames.minimine import Minimine
 # -¤- #
 from block import Block
 from buttonsBlock import ButtonsBlock
 
 
-# Not usable yet:
-# from minigames.minimine import Minimine
-
-
 class GUI(tk.Frame):  # Inheritance, hierarchy
+
     def __init__(self, parent):
         super().__init__()  # Calls the parent-class initializer.
+        self.game = Memorygame()
+
         self.parent = parent
-        self.parent.geometry("400x400")
+        self.parent.geometry("930x325")
         self.parent.title("This should get overwritten!")
 
-        self.game = FiveRow()
         self.parent.title(self.game.title)  # Assign title according to parent.
 
         self.buttons = ButtonsBlock(parent=self)  # ButtonsBlock's parent == this self
         self.__createLayout()
 
     def __createLayout(self):
-        x, y = 0, 0
-        self.buttons.grid(row=x, column=y, sticky=tk.S, padx=10, pady=10)
+        self.buttons.grid(row=0, column=0, sticky=tk.NSEW)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.buttons._ButtonsBlock__createLayout()
+        # x, y = 0, 0
+        # self.buttons.grid(row=x, column=y, sticky=tk.S, padx=10, pady=10)
 
     def move(self, place):
         """
         Check if game is over.
         :return:
         """
-        self.game.move(place)
-        if self.game.isGameOver() != 0:
-            self.buttons.disableAll()
+        self.buttons.disable(place)
 
-            # After disabling buttons, query user:
-            if tk.messagebox.askyesnocancel("NEW GAME", "GAME OVER " + self.game.title):
+        # a.k.a. if this 'isInstance' sentence == True
+        if isinstance(self.game, Memorygame):  # Game : Memorygame
+            place1 = place2 = -1
+            if len(self.game.pair) == 2 and \
+                    self.game.pair[0] != self.game.pair[1] and \
+                    self.game.pair[0] in self.game.marks and \
+                    self.game.pair[1] in self.game.marks:
+                place1 = self.game.board.index(self.game.pair[0])
+                place2 = self.game.board.index(self.game.pair[1])
+                self.buttons.buttons[place1].config(state="normal")
+                self.buttons.buttons[place2].config(state="normal")
+            self.game.move(place)
+            if place1 != -1 and place2 != -1:  #
+                self.buttons.tvar[place1].set(self.game.board[place1])
+                self.buttons.tvar[place2].set(self.game.board[place2])
+
+        elif isinstance(self.game, FiveRow):  # Game : FiveRow
+            machine = " O "
+            self.game.move(place)
+            for i, item in enumerate(self.game.board):
+                if item == machine:
+                    self.buttons.tvar[i].set(self.game.board[i])
+                    self.buttons.disable(i)
+
+        else:  # Game : Minimine
+            self.game.move(place)
+
+        self.buttons.tvar[place].set(self.game.board[place])  # update buttons
+        # is game over
+        situation = self.game.isGameOver()
+        if situation != 0:
+            self.buttons.disableAll()  # game over
+            if tk.messagebox.askyesno('New game?', self.game.title):
                 self.game.reset()
-
                 self.buttons.enableAll()
+                self.__createLayout()
             else:
-                self.parent.destroy()  # Calls destroy() from parent, which here is self.
+                self.parent.destroy()  # destroy window
+        # if self.game.isGameOver() != 0:
+        #     self.buttons.disableAll()
+        #
+        #     # After disabling buttons, query user:
+        #     if tk.messagebox.askyesnocancel("NEW GAME", "GAME OVER " + self.game.title):
+        #         self.game.reset()
+        #
+        #         self.buttons.enableAll()
+        #     else:
+        #         self.parent.destroy()  # Calls destroy() from parent, which here is self.
 
 
 # A safeguard and also a sign for coder that this is something to run.
 if __name__ == '__main__':
     window = tk.Tk()
     frame = GUI(parent=window)
-    frame.grid(row=0, column=0)
+    frame.grid(row=0, column=0, sticky=tk.NSEW)
+    window.rowconfigure(0, weight=1)
+    window.columnconfigure(0, weight=1)
     window.mainloop()
